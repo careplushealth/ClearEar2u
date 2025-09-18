0f7c493e9dd8bec0b85c05173ec922cd0196fd49
macbook <macbook@macbooks-MacBook-Pro.local>
2025-09-18 13:21:43 +0100

    redirect fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <div className="page-container">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="#pricing" element={<Pricing />} />
+          <Route path="/pricing" element={<Pricing />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
